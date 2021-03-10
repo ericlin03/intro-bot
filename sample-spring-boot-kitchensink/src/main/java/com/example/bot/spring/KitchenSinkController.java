2a8c53967ccd2609c2563d169e6effe0fe0a3b07
ericlin03 <you@example.com>
2021-03-10 14:25:03 +0800

    change message action to postback action

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -383,14 +383,17 @@ public class KitchenSinkController {
                         Arrays.asList(
                                 new ImageCarouselColumn(CTBC,
                                                         new PostbackAction("CTBC intern",
+                                                                          "CTBC intern",
                                                                           "App Security Intern\n● Responsible for the black- and white-box testing of over 20 systems\n● Built environment of white-box testing, imported policy package\n● Updated policy package of black-box testing, recorded scripts of black-box testing\n● Pre-reviewed vulnerability of systems before online")
                                 ),
                                 new ImageCarouselColumn(microsoft,
                                                         new PostbackAction("Careerhack",
+                                                                          "Careerhack",
                                                                           "2021 Microsoft & TSMC Careerhack\n● Be shortlisted for the final contest and built a online chatbot with Azure\n● Responsible for version control, Database, and application deployment")
                                 ),
                                 new ImageCarouselColumn(fju,
                                                         new PostbackAction("FJU project",
+                                                                          "FJU project",
                                                                           "Blockchain Ticketing Platform and Payment Project\n● Built private blockchain with Ethereum\n● Wrote smart contract and deployed on blockchain with Solidity\n● Wrote API for website and blockchain with JavaScript")
                                 )
                         ));
@@ -425,15 +428,17 @@ public class KitchenSinkController {
                         Arrays.asList(
                                 new ImageCarouselColumn(trip,
                                                         new PostbackAction("Yilan Trip",
-                                                                          "test",
+                                                                          "Yilan Trip",
                                                                           flying_umbrella)
                                 ),
                                 new ImageCarouselColumn(tainan,
                                                         new PostbackAction("Tainan Trip",
+                                                                          "Tainan Trip",
                                                                           "This picture was taken in Tainan Museum!!!")
                                 ),
                                 new ImageCarouselColumn(careerhack,
                                                         new PostbackAction("Careerhack",
+                                                                          "Careerhack",
                                                                           "Hack day in Microsoft Office")
                                 )
                         ));
